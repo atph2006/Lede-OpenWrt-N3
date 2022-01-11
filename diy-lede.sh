ef0398c157942735c4cec276ce11d46f59278a5b
老C(ㆁωㆁ*)²³³ <13417924+iPixelOldC@users.noreply.github.com>
2022-01-11 23:26:43 +0800

    test2

--- diy-lede.sh
+++ diy-lede.sh
@@ -27,8 +27,9 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowS
 echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git  clone https://github.com/kenzok8/openwrt-packages package/
-
+git clone https://github.com/kenzok8/openwrt-packages opks
+mv opks/* package/
+rm -rf opks/
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom  #透明主题
 #git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
