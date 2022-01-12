6bfeaa0f4c71de0c74887b6566b9a297c0fc1a26
老C(ㆁωㆁ*)²³³ <13417924+iPixelOldC@users.noreply.github.com>
2022-01-12 22:48:57 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -27,27 +27,27 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowS
 echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone https://github.com/kenzok8/openwrt-packages opks
-mv opks/* package/
-rm -rf opks/
+#git clone https://github.com/kenzok8/openwrt-packages opks
+#mv opks/* package/
+#rm -rf opks/
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom  #透明主题
-#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
-#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
 #git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial #atmaterial主题
 #luci-app-compressed-memory 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter #应用过滤/家长控制
 
 #passwall出国软件
-#git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
 
 #Hello World
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb #git lua-maxminddb 依赖
-#git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 
 #xray
 git clone https://github.com/yichya/luci-app-xray package/luci-app-xray
